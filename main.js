50e33a8f07491119532bc11d4511ff9137e81526
mohammad-rakib-hasan <bdprakib9@gmail.com>
2024-10-19 23:39:30 +0600

    my if else

---
+++ main.js
@@ -0,0 +1,92 @@
+let marks;
+
+marks =95;
+
+if(marks >100|| marks <0){
+    console.log('Invalid marks');
+}
+
+else if(marks >=80){
+    console.log('The grade is: A+');
+}
+
+else if (marks >=70){
+    console.log('This is grade: A');
+}
+
+else if (marks >=60){
+    console.log('This is grade: A-');
+}
+
+else if (marks >=50){
+    console.log('This is grade: B');
+}
+
+else if (marks >=40){
+    console.log('This is grade: C');
+}
+
+else if (marks >=33){
+    console.log('This is grade: D');
+}
+
+else{
+    console.log('This grade is: F')
+}
+
+// let number;
+
+// number = 246821898499995;
+
+// if(number % 2 ==0){
+//     console.log("This is an even number");
+// }
+
+// else{
+//     console.log("This is an odd number");
+// }
+
+// let number1 = 500005505050;
+// let number2 = 565696569560;
+
+// if(number1 > number2){
+//     console.log("Number1 is the largest")
+// }
+
+// else{
+//     console.log("Number2 is the largest")
+// }
+
+// let number = -700;
+
+// if(number > 0){
+//     console.log("This number is postive");
+// }
+
+// else if(number < 0){
+//     console.log("This number is negative");
+// }
+
+// else{
+//     console.log("This is zero");
+
+// }
+
+// let number =30;
+// if(number % 6 == 0 || number % 11 ==0){
+//     console.log("The number is divisible by 6 and 11");
+// }
+
+// else{
+//     console.log("This number is not divisble by 6 and 11");
+// }
+
+let year =2022;
+
+if(year % 4 ==0){
+    console.log("This is leap year");
+}
+
+else{
+    console.log("This is not leap year");
+}
